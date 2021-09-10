0c21be770ea6af6e8a45eb689d25908184b2f5b7
Nithin Joy <nithinjoy2020@gmail.comgit config --global user.email nithinjoy2020@gmail.com>
2021-09-10 15:25:58 +1200

    first

---
+++ MVCLogin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVCLogin.Areas.Identity.Data;
+using MVCLogin.Data;
+
+[assembly: HostingStartup(typeof(MVCLogin.Areas.Identity.IdentityHostingStartup))]
+namespace MVCLogin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MVCDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MVCDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>  { options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<MVCDBContext>();
+            });
+        }
+    }
+}
